a87cb6fdc0851ba71b8944eca8db5bca5ea3e38e
Juan Paz <100331400+JuanFPaz@users.noreply.github.com>
2023-11-21 18:51:28 -0300

    App.jsx / modificando src de la imagen

--- portfolio-jpaz/src/App.jsx
+++ portfolio-jpaz/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
             >
               <img
                 id="logo-img"
-                src="https://raw.githubusercontent.com/JuanFPaz/JuanFPaz.github.io/main/portfolio-jpaz-react/public/jp-logo-home-10.png"
+                src="https://raw.githubusercontent.com/JuanFPaz/JuanFPaz.github.io/main/portfolio-jpaz/public/jp-logo-home-10.png"
                 alt="jp - logo home"
               />
             </a>
