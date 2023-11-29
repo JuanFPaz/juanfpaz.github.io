f514958576ae6dafcb59538bbb77f40ff63192eb
Juan Paz <jpaz.dev94@gmail.com>
2023-11-29 02:20:00 -0300

    Fixeando img

--- portfolio-jpaz/src/App.jsx
+++ portfolio-jpaz/src/App.jsx
@@ -1,28 +1,28 @@
+import jpazLogo from './assets/jp-logo-home-10.png'
+
 function App() {
   return (
-    <>
-      <main>
-        <section className="content" id="Inicio">
-          <h1>Disculpe las molestias</h1>
-          <p>Me encuentro creando nuevo contenido para mi sitio web.</p>
-          <p>Nos vemos pronto.</p>
-          <div className="container">
-            <a
-              id="logo"
-              href="https://github.com/JuanFPaz"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img
-                id="logo-img"
-                src="https://raw.githubusercontent.com/JuanFPaz/JuanFPaz.github.io/main/portfolio-jpaz/public/jp-logo-home-10.png"
-                alt="jp - logo home"
-              />
-            </a>
-          </div>
-        </section>
-      </main>
-    </>
+    <main>
+      <section className="content" id="Inicio">
+        <h1>Disculpe las molestias</h1>
+        <p>Me encuentro creando nuevo contenido para mi sitio web.</p>
+        <p>Nos vemos pronto.</p>
+        <div className="container">
+          <a
+            id="logo"
+            href="https://github.com/JuanFPaz"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              id="logo-img"
+              src={jpazLogo}
+              alt="jp - logo home"
+            />
+          </a>
+        </div>
+      </section>
+    </main>
   );
 }
 
